1fc704349f957c1794d41f8c9f9d1c5e3d462f7a
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2024-04-16 20:56:02 +0700

    feat: update deployment config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
-require("@openzeppelin/hardhat-upgrades");
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
--- ignition/modules/Vote.ts
+++ ignition/modules/Vote.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const _ownerAddress = 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266;
+const _ownerAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 const _candidates = ["Candidate 1", "Candidate 2", "Candidate 3"];
 
 const VoteModule = buildModule("VoteModule", (m) => {
